58be41f76ae2761fa0eb56c6568a8eef432e6e9e
vladd12 <vladd12z32@gmail.com>
2020-08-06 21:08:09 +0300

    Update 07. Using for (part two).cpp

--- Learning C++/07. Using for (part two).cpp
+++ Learning C++/07. Using for (part two).cpp
@@ -1,4 +1,4 @@
-//тип char (обратный алфавит)
+//С‚РёРї char (РѕР±СЂР°С‚РЅС‹Р№ Р°Р»С„Р°РІРёС‚)
 #include "stdafx.h"
 #include <iostream>
 #include <cstdlib>
@@ -11,7 +11,7 @@ int main()
 	setlocale(LC_ALL, "ru_RU.UTF-8");
 	setlocale(LC_ALL, "");
 	char letter;
-	cout << "Программа вывода английского алфавита в обратном порядке\n" << endl;
+	cout << "РџСЂРѕРіСЂР°РјРјР° РІС‹РІРѕРґР° Р°РЅРіР»РёР№СЃРєРѕРіРѕ Р°Р»С„Р°РІРёС‚Р° РІ РѕР±СЂР°С‚РЅРѕРј РїРѕСЂСЏРґРєРµ\n" << endl;
 	for (letter = 'Z'; letter >= 'A'; letter--)
 		cout << letter << endl;
 	system("pause");
