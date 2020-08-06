33f601143c217c70c81b56b909130db780752dd6
vladd12 <vladd12z32@gmail.com>
2020-08-06 21:09:29 +0300

    Update 09. Zone of using variables.cpp

--- Learning C++/09. Zone of using variables.cpp
+++ Learning C++/09. Zone of using variables.cpp
@@ -1,4 +1,4 @@
-//инициализация переменных
+//РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РїРµСЂРµРјРµРЅРЅС‹С…
 #include <iostream>
 #include <cstdlib>
 #include <Windows.h>
@@ -11,9 +11,9 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	setlocale(LC_ALL, "ru_RU.UTF-8");
 	setlocale(LC_ALL, "");
-	cout << "Вычисление суммы чисел от 1 до 5.\n" << endl;
+	cout << "Р’С‹С‡РёСЃР»РµРЅРёРµ СЃСѓРјРјС‹ С‡РёСЃРµР» РѕС‚ 1 РґРѕ 5.\n" << endl;
 	total(5);
-	cout << "\nВычисление суммы чисел от 1 до 6.\n" << endl;
+	cout << "\nР’С‹С‡РёСЃР»РµРЅРёРµ СЃСѓРјРјС‹ С‡РёСЃРµР» РѕС‚ 1 РґРѕ 6.\n" << endl;
 	total(6);
 	system("pause");
 	return 0;
@@ -21,12 +21,12 @@ int main()
 
 void total(int x)
 {
-	int sum = 0; //инициализация переменной sum
+	int sum = 0; //РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РїРµСЂРµРјРµРЅРЅРѕР№ sum
 	int i, count;
 	for (i = 1; i <= x; i++)
 	{
 		sum = sum + i;
 		for (count = 0; count < 10; count++) cout << '.';
-		cout << "Промежуточная сумма равна " << sum << '\n' << endl;
+		cout << "РџСЂРѕРјРµР¶СѓС‚РѕС‡РЅР°СЏ СЃСѓРјРјР° СЂР°РІРЅР° " << sum << '\n' << endl;
 	}
 }
